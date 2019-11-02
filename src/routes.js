32e5412e31e0514e16c50022b99e56add2a1d010
David Costa <davidcostadev@gmail.com>
2019-11-02 09:10:03 -0300

    Fix import

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const { Resources } = require('fastexpress');
-const Tasks = require('./resources/tasks');
+const Tasks = require('./resources/Tasks');
 
 const routers = new Resources({
   namespace: '/api/v1/',
